26ace022988e1ed355d4fbc5a0ff3dcad100c534
gochxx <falko.trischler@web.de>
2025-05-18 15:13:02 +0200

    more tracking to experiments added

--- 22_experiments.py
+++ 22_experiments.py
@@ -5,14 +5,21 @@ from sklearn.metrics import accuracy_score
 from google.cloud import bigquery
 import pandas as pd
 import joblib
+import os
+import tempfile
 
+
+PROJECT_ID = "vertex2025"
+REGION = "us-central1"
+EXPERIMENT_NAME = "rf-experiment-05"
+RUN_NAME = "run1"
 
 # Init
-aiplatform.init(experiment="rf-experiment-04", project="vertex2025", location="us-central1")
-aiplatform.start_run(run="run1")
+aiplatform.init(experiment=EXPERIMENT_NAME, project=PROJECT_ID, location=REGION)
+aiplatform.start_run(run=RUN_NAME)
 
 # Daten aus BigQuery
-client = bigquery.Client(project="vertex2025")
+client = bigquery.Client(project=PROJECT_ID)
 df = client.query("SELECT * FROM `vertex2025.gochx_data.gochx_table`").to_dataframe()
 
 X = df.drop(columns=["label"]).values
@@ -28,5 +35,24 @@ accuracy = accuracy_score(y_test, y_pred)
 # Logs
 aiplatform.log_params({"n_estimators": 100, "random_state": 42})
 aiplatform.log_metrics({"accuracy": accuracy})
+
+''' MUSS NOCH AKTUALISIERT WERDEN
+Siehe: https://cloud.google.com/vertex-ai/docs/experiments/track-executions-artifacts?hl=de
+# Save and log the model as artifact
+with tempfile.TemporaryDirectory() as tmpdir:
+    model_path = os.path.join(tmpdir, "model.joblib")
+    joblib.dump(model, model_path)
+    aiplatform.log_artifact(model_path, artifact_id="rf_model")
+
+# log output predictions
+results_df = pd.DataFrame({
+    "y_true": y_test,
+    "y_pred": y_pred
+})
+with tempfile.TemporaryDirectory() as tmpdir:
+    results_path = os.path.join(tmpdir, "predictions.csv")
+    results_df.to_csv(results_path, index=False)
+    aiplatform.log_artifact(results_path, artifact_id="predictions")
+'''
 
 aiplatform.end_run()
